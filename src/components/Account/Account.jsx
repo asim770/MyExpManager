c0254f8f64adf311a4b8d86d78c6a893421cb3e4
asim770 <majiasim.1234@gmail.com>
2025-09-15 12:10:49 +0530

    add database

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -9,25 +9,33 @@ export default function Account(){
         setAmount(e.target.value);
     }
 
-    function amountAdded(){
-        if(amount==0){
-            alert("ENTER SOME AMOUNT");
-            navigate("/")
-        }
-        else{
-            localStorage.setItem("Amount",amount);
-            navigate("/profile");
+    async function amountAdded(e){
+        e.preventDefault();
+        const response=await fetch("http://localhost:8000/account",{
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                amount: amount
+            })
+        })
+        const result= response.json;
+        if(response.ok){
+            alert("✅ Registration successful!");
+            setAmount("");
+        }else {
+            alert("❌ " + result.msg);
         }
     }
     return(
         <>
         <div className="h-screen flex justify-center items-center">
-            <div>
-                Enter Your Amount
+            <div className="bg-[#696969] h-auto flex flex-col gap-8 p-[35px] border-[1px] border-white rounded-[20px] shadow-2xl shadow-[#000000]">
+                
+                <h1 className="text-[20px] font-bold text-amber-50 ">Enter your Amount</h1>
 
-                <input type="text" value={amount} onChange={amountUpdate}/>
+                <input type="text " className="bg-amber-50 h-[40px] w-[200px] p-[10px] rounded-[20px] border-none hover:shadow-black hover:shadow-2xl duration-300 transition-colors" placeholder="Enter Your Amount"  value={amount} onChange={amountUpdate}/>
 
-                <button onClick={amountAdded}>submit</button>
+                <button onClick={amountAdded} className="px-6 py-3 rounded-[25px] border-[1px] border-[#b4b4b461] bg-gray-700 text-gray-100 font-semibold hover:bg-gray-600 hover:text-white transition-colors duration-300 shadow-md ">submit</button>
                 
             </div>
         </div>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -73,32 +73,33 @@ export default function Profile() {
 
   return (
     <>
-      <div>
+      <div className="flex flex-row p-[40px] md:justify-between gap-1 w-screen items-center md:w-full">
         <input
           type="text"
-          className="bg-white text-black m-2"
+          className="bg-white text-black h-[40px]  w-auto p-[10px] rounded-full hover:shadow-2xl hover:shadow-[#646060] transition-colors duration-300"
           value={item}
           onChange={addItem}
           placeholder="Enter item"
         />
         <input
           type="number"
-          className="bg-white text-black m-2"
+          className="bg-white text-black h-[40px] p-[10px] rounded-full hover:shadow-2xl hover:shadow-[#646060] transition-colors duration-300"
           value={price}
           onChange={addPrice}
           placeholder="Enter price"
         />
-        <button onClick={createList} className="bg-white text-black">
+        <button onClick={createList} className="px-[10px] h-[40px] rounded-[25px] border-[1px] border-[#b4b4b461] bg-gray-700 text-gray-100 font-semibold hover:bg-gray-600 hover:text-white transition-colors duration-300 shadow-md">
           Confirm
         </button>
       </div>
-
-      <div className="m-4">
-        <p>Total Amount: {userAmount}</p>
-        <p>Spent: {expandAmount}</p>
-        <p>Balance: {balanceAmount}</p>
+      <div className="m-4 flex md:justify-between md:px-[50px] flex-col md:flex-row gap-10 justify-center items-center">
+        <p className="h-[150px] text-amber-50 bg-[#5b5a5a] w-[300px] p-[15px] border-[1px] text-[20px] font-bold rounded-[20px] shadow-xl/30  border-amber-50 ">
+          Total Amount: <br /> <br /> <span>₹{userAmount}</span></p>
+        <p className="h-[150px] text-amber-50 bg-[#5b5a5a] w-[300px] p-[15px] border-[1px] text-[20px] font-bold rounded-[20px] shadow-xl/30  border-amber-50 ">Spent: <br />
+        <br /><span>₹{expandAmount}</span></p>
+        <p className="h-[150px] text-amber-50 bg-[#5b5a5a] w-[300px] p-[15px] border-[1px] text-[20px] font-bold rounded-[20px] shadow-xl/30  border-amber-50 ">Balance: <br />
+        <br /><span>₹{balanceAmount}</span></p>
       </div>
-
       <div>
         {expendatures.map((item, index) => (
           <div key={index}>
